9db6670b041f3e33676a1f509f49d9404d3e9fd3
zombief0 <theendisnearlicence3>
2021-03-11 11:43:08 +0100

    init

---
+++ src/main/java/com/example/springtestless/SpringtestlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestlessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtestless/SpringtestlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
